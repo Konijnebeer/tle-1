ab4ebee4ce6c2286906f4e97730b072e7321fb76
Konijnebeer <98305413+Konijnebeer@users.noreply.github.com>
2025-09-08 14:28:51 +0200

    component folder

---
+++ src/components/PopUp.jsx
@@ -0,0 +1,16 @@
+// import { cn } from "@/lib/utils";
+// import { Bug } from "lucide-react";
+
+function Virus() {
+  return (
+    <div className="w-20 h-15 bg-blue-500">
+      <div>
+        {/* <div className={cn("bg-red-500", "before:bg-pink-600")}>
+          <Bug />
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export { Virus };
